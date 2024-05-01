34f27946201bae3fc3117a8ff63d7a691f0b9e92
hotTeaFun <1767230254@qq.com>
2024-05-02 01:23:20 +0800

    Update SuffixArray.

--- sa.cpp
+++ sa.cpp
@@ -1,50 +1,58 @@
 #include <bits/stdc++.h>
 
+using i64 = long long;
 struct SuffixArray {
+  int n;
   // 后缀i:s[i...n]
   // sa[i] 表示将所有后缀排序后第 i 小的后缀的编号
   // rk[i] 表示后缀 i 的排名
-  // height[i]=lcp(sa[i],sa[i-1])，即第 i 名的后缀与它前一名的后缀的最长公共前缀
-  // height[1]=0
-  std::vector<int> sa, rk, height;
-  // s[1...n]为实际字符串,s[0]留空
+  // lc[i]=lcp(sa[i],sa[i-1])，即第 i 名的后缀与它前一名的后缀的最长公共前缀
+  // lc[1]=0
+  std::vector<int> sa, rk, lc;
   SuffixArray(const std::string& s) {
-    int n = s.size() - 1;
-    sa = std::vector<int>(n + 1);
-    rk = std::vector<int>(2 * n + 1);
-    std::vector<int> oldrk(2 * n + 1);
-    for (int i = 1; i <= n; ++i) sa[i] = i, rk[i] = s[i];
-    for (int w = 1; w < n; w <<= 1) {
-      std::sort(next(begin(sa)), end(sa), [&](int x, int y) {
-        return rk[x] == rk[y] ? rk[x + w] < rk[y + w] : rk[x] < rk[y];
-      });
-      std::copy(begin(rk), end(rk), begin(oldrk));
-      for (int p = 0, i = 1; i <= n; ++i) {
-        if (oldrk[sa[i]] == oldrk[sa[i - 1]] &&
-            oldrk[sa[i] + w] == oldrk[sa[i - 1] + w]) {
-          rk[sa[i]] = p;
-        } else {
-          rk[sa[i]] = ++p;
-        }
-      }
+    n = s.length();
+    sa.resize(n);
+    lc.resize(n - 1);
+    rk.resize(n);
+    std::iota(sa.begin(), sa.end(), 0);
+    std::sort(sa.begin(), sa.end(), [&](int a, int b) { return s[a] < s[b]; });
+    rk[sa[0]] = 0;
+    for (int i = 1; i < n; ++i)
+      rk[sa[i]] = rk[sa[i - 1]] + (s[sa[i]] != s[sa[i - 1]]);
+    int k = 1;
+    std::vector<int> tmp, cnt(n);
+    tmp.reserve(n);
+    while (rk[sa[n - 1]] < n - 1) {
+      tmp.clear();
+      for (int i = 0; i < k; ++i) tmp.push_back(n - k + i);
+      for (auto i : sa)
+        if (i >= k) tmp.push_back(i - k);
+      std::fill(cnt.begin(), cnt.end(), 0);
+      for (int i = 0; i < n; ++i) ++cnt[rk[i]];
+      for (int i = 1; i < n; ++i) cnt[i] += cnt[i - 1];
+      for (int i = n - 1; i >= 0; --i) sa[--cnt[rk[tmp[i]]]] = tmp[i];
+      std::swap(rk, tmp);
+      rk[sa[0]] = 0;
+      for (int i = 1; i < n; ++i)
+        rk[sa[i]] = rk[sa[i - 1]] + (tmp[sa[i - 1]] < tmp[sa[i]] ||
+                                     sa[i - 1] + k == n ||
+                                     tmp[sa[i - 1] + k] < tmp[sa[i] + k]);
+      k *= 2;
     }
-    height = std::vector<int>(n + 1);
-    for (int i = 1, k = 0; i <= n; ++i) {
-      if (rk[i] == 0) continue;
-      if (k) --k;
-      while (s[i + k] == s[sa[rk[i] - 1] + k]) ++k;
-      height[rk[i]] = k;
+    for (int i = 0, j = 0; i < n; ++i) {
+      if (rk[i] == 0) {
+        j = 0;
+      } else {
+        for (j -= j > 0; i + j < n && sa[rk[i] - 1] + j < n &&
+                         s[i + j] == s[sa[rk[i] - 1] + j];)
+          ++j;
+        lc[rk[i] - 1] = j;
+      }
     }
   }
 };
 int main() {
-  std::string s;
-  std::cin >> s;
-  s.insert(begin(s), '0');
-  int n = s.size() - 1;
-  SuffixArray sa(s);
-  int res = n * (n + 1) / 2 -
-            std::accumulate(next(begin(sa.height), 2), end(sa.height), 0);
-  std::cout << res << "\n";
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
   return 0;
 }
